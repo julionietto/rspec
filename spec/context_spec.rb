8398856437b4830ef27e25c8a051a004a572974a
Julio <julionietto@gmail.com>
2022-01-06 14:44:41 -0300

    comitando context_spec.rb

---
+++ spec/context_spec.rb
@@ -0,0 +1,11 @@
+describe 'FGTS' do
+
+    context 'Multa' do
+        it 'calculando multa' do
+            vlFgts  = 161104
+            vlMulta = vlFgts * 0.40
+            puts "Valor da Multa FGTS = #{vlMulta}"
+        end
+    end
+
+end
