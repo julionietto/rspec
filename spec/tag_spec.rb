9bf09c81c6777cd81331cbc4a1b695c9191bf8b9
Julio <julionietto@gmail.com>
2022-01-06 18:03:51 -0300

    Atualizando projeto

---
+++ spec/tag_spec.rb
@@ -0,0 +1,23 @@
+describe 'Teste um', teste_um: true do
+    it 'teste 1' do
+        puts 'teste um sendo executado'
+    end
+end
+
+describe 'Teste dois', :teste_dois do
+    it 'teste 2' do
+        puts 'teste dois sendo executado'
+    end
+end
+
+describe 'Teste tres', :teste_tres do
+    it 'teste 3' do
+        puts 'teste tres sendo executado'
+    end
+end
+
+describe 'Teste quatro', :teste_quatro do
+    it 'teste 4' do
+        puts 'teste quatro sendo executado'
+    end
+end
